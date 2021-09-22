f33e6f248a231c0e8cda0fe143277b39738f1c16
Lily Ma <xma50089@gmail.com>
2021-09-22 08:50:59 -0500

    Added question description and documentation to max_stack

--- max_stack.py
+++ max_stack.py
@@ -1,19 +1,29 @@
+# Question: Max in a stack problem overview
+# The aim is to design an algorithm that can return maximum item of a stack in O(1) running time complexity.
+# We can use O(N) extra memory
+
 class MaxStack:
 
     def __init__(self):
-        # Use one stack for operation
+        # Use one stack for adding new item
         self.main_stack = []
-        # Use another stack for the  operation
+        # Use another stack to get the max item
         self.max_stack = []
 
     # Adding an item to the  - O(1) constant time
     def push(self, data):
+        # push the new item onto the stack
         self.main_stack.append(data)
+
+        # First item is teh same in both stacks
         if len(self.main_stack) == 1:
             self.max_stack.append(data)
         else:
+            # If the new item smaller than the largest one so far,
+            # then we duplicate the largest one on the max-stack
             if data < self.max_stack[-1]:
                 self.max_stack.append(self.max_stack[-1])
+            # If the item is the largest one so far, we insert it onto the stack
             else:
                 self.max_stack.append(data)
 
@@ -22,7 +32,6 @@ class MaxStack:
         return self.max_stack.pop()
 
 
-# if __name__='__main__'
 max_stack = MaxStack()
 max_stack.push(10)
 max_stack.push(5)
