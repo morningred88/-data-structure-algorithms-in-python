701378c633b2cf4765515e81b5a685d7e141f92f
Lily Ma <xma50089@gmail.com>
2021-09-27 21:13:57 -0500

    Added factorial using recursion

---
+++ Factorial.py
@@ -0,0 +1,17 @@
+class Factorial:
+    """
+    This class demonstrate the factorial of a given number using recursion
+    """
+
+    def fact(self, number):
+
+        # Base case
+        if number == 0:
+            return 1
+        else:
+            return number * self.fact(number-1)
+
+
+factorial = Factorial()
+print(factorial.fact(5))
+
