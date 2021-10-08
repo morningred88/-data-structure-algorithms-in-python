a269d4a57021dfd607748795a25eb368a0393dec
Lily Ma <xma50089@gmail.com>
2021-10-08 08:28:27 -0500

    Added question for dutch flag partition problem

---
+++ EPI/ch5.1_dutch_flag_partition.py
@@ -0,0 +1,8 @@
+"""
+Question:
+Write a program that takes an integer array (A) and an index i into A,  rearrange the elements such that all elements less than A[i] appear first,  at the beginning, followed by elements equal to A[i], then elements greater than A[i].
+
+For example:
+A =[5, 6, 2, 7, 8, 3, 6, 2, 4, 5], A[i]= 5, output [2, 3, 4, 5, 5, 6, 7, 8, 6] is a valid output
+A =[5, 6, 2, 7, 8, 3, 6, 2, 4, 5], A[i]= 7, output [2, 3, 4, 5, 5, 6, 6, 7, 8] is a valid output
+"""
