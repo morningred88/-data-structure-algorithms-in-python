39bc4fe58d233fc9e9a4baaf55d8f81780d71a83
Lily Ma <xma50089@gmail.com>
2021-10-09 18:58:00 -0500

    Added three key partition question

---
+++ EPI/ch5.1v_three_keys_partition.py
@@ -0,0 +1,4 @@
+"""
+Variation question:
+Assuming the keys take one of three values, recorder the array so that the keys with the same value appear together. The order of the subarrays are not important. Use O(1) space and O(n) time.
+"""
