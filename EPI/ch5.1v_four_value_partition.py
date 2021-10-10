ccb693947fd9d109775fb4949190165f38d1d263
Lily Ma <xma50089@gmail.com>
2021-10-09 21:42:39 -0500

    Added four value partitions problem

---
+++ EPI/ch5.1v_four_value_partition.py
@@ -0,0 +1,4 @@
+"""
+Variation question 2
+Given an array A of n objects with keys that takes one of four values, reorder the arrays so that all objects that have the same key appear together. Use O(1) space and O(n) time
+"""
--- EPI/ch5.1v_three_keys_partition.py
+++ EPI/ch5.1v_three_keys_partition.py
@@ -1,5 +1,5 @@
 """
-Variation question:
+Variation question 1:
 Assuming the keys take one of three values, recorder the array so that the keys with the same value appear together. The order of the subarrays are not important. Use O(1) space and O(n) time.
 
 """
