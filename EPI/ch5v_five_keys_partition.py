138af30f601991d5b7641866c4f8d90534de6e04
Lily Ma <xma50089@gmail.com>
2021-10-10 18:11:14 -0500

    Updated format and corrected typo for five keys partition

--- EPI/ch5v_five_keys_partition.py
+++ EPI/ch5v_five_keys_partition.py
@@ -6,30 +6,34 @@ This question is used to prove the explanation I wrote for partition problems in
 """
 from typing import List
 
-def five_keys_partition(A:List[int], first: int, second: int, third: int, forth: int, fifth: int)-> List:
 
-    first_key, second_key, third_key, forth_key, fifth_key = 0, 0, 0, 0, len(A)
+def five_keys_partition(A:List[int], first: int, second: int, third: int, fourth: int, fifth: int)-> List:
 
-    while (forth_key < fifth_key):
-        if A[forth_key] == first:
-            A[first_key], A[forth_key] = A[forth_key], A[first_key]
-            A[forth_key], A[second_key] = A[second_key], A[forth_key]
-            A[forth_key], A[third_key] = A[third_key], A[forth_key]
-            first_key, second_key, third_key, forth_key= first_key +1, second_key +1, third_key +1, forth_key+1
-        elif A[forth_key] == second:
-            A[second_key], A[forth_key] = A[forth_key], A[second_key]
-            A[forth_key], A[third_key] = A[third_key], A[forth_key]
-            second_key, third_key, forth_key = second_key + 1, third_key + 1, forth_key+1
-        elif A[forth_key] == third:
-            A[forth_key], A[third_key] = A[third_key], A[forth_key]
-            third_key, forth_key = third_key + 1, forth_key+1
-        elif A[forth_key] == forth:
-            forth_key +=1
+    first_key, second_key, third_key, fourth_key, fifth_key = 0, 0, 0, 0, len(A)
+
+    while fourth_key < fifth_key:
+        if A[fourth_key] == first:
+            A[first_key], A[fourth_key] = A[fourth_key], A[first_key]
+            A[fourth_key], A[second_key] = A[second_key], A[fourth_key]
+            A[fourth_key], A[third_key] = A[third_key], A[fourth_key]
+            first_key, second_key, third_key, fourth_key = first_key +1, second_key +1, third_key +1, fourth_key+1
+        elif A[fourth_key] == second:
+            A[second_key], A[fourth_key] = A[fourth_key], A[second_key]
+            A[fourth_key], A[third_key] = A[third_key], A[fourth_key]
+            second_key, third_key, fourth_key = second_key + 1, third_key + 1, fourth_key+1
+        elif A[fourth_key] == third:
+            A[fourth_key], A[third_key] = A[third_key], A[fourth_key]
+            third_key, fourth_key = third_key + 1, fourth_key+1
+        elif A[fourth_key] == fourth:
+            fourth_key += 1
         else:
-            fifth_key -=1
-            A[forth_key], A[fifth_key] =A[fifth_key], A[forth_key]
+            # A[fifth_key] == fifth
+            fifth_key -= 1
+            A[fourth_key], A[fifth_key] =A[fifth_key], A[fourth_key]
 
     return A
 
-print(five_keys_partition([2, 1, 1, 2, 4, 3,5, 1, 2, 5, 1, 2, 4, 2, 3], 1, 2, 3, 4, 5))
+
+array = [2, 1, 1, 2, 4, 3, 5, 1, 2, 5, 1, 2, 4, 2, 3]
+print(five_keys_partition(array, 1, 2, 3, 4, 5))
 
