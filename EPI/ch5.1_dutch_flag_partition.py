93ecef348ef6abac1c358adf1bb4ae141f5417cb
Lily Ma <xma50089@gmail.com>
2021-10-09 21:36:25 -0500

    Updated dutch flat partition explanation

--- EPI/ch5.1_dutch_flag_partition.py
+++ EPI/ch5.1_dutch_flag_partition.py
@@ -11,16 +11,34 @@ A =[5, 6, 2, 7, 8, 3, 6, 2, 4, 5], A[3]= 7, output [2, 3, 4, 5, 5, 6, 6, 7, 8] i
 Explanation:
 Think about the partition step in quicksort. A[i] as the pivot.
 
+There are 4 apporaches to solve the problem
+
+First approach:
+Build 3 arrays, one less than the pivot, one equal to pivot and one array greater than pivot. Then we will write the 3 arrays back to array A. O(N) space and 0(N) time.
+
+Second approach:
+We can do this in place and keep the space constant. We don't want to create space that scales with out input. Improve the space complexity from O(N) to O(1). 
 Since space complexity is O(1), it must be some swap step happened in order to achieve the rearrangement. 
 
-Bruteforce:
+We do this using 2 passes. First pass to find all the elements less than pivot. Second pass to find all the elements greater than pivot. 
+
 First pass - move all the element less than A[i] to the first partition: Consider one element after another, starting from first element A[0]. Loop through and  find the first element first element A[j] less than A[i]. The move to the second element, find the next element A[j] less than A[i]. It tooks 2 for loops to find all the elements less than A[i].
 Second pass- move all the element greater than A[i] to the last partition: We use the same method as for moving all the element less than A[i]. But this time starting from the last element of the array A[len(A)-1). It also tooks 2 for loops to find all the elements greater than A[i].
 Time Complexity O(N^2), space complexity O(1)
 
-Optimized Solution:
+Third approach: improve the time complexity from the second approach 
+We still do this using 2 passes. But we will remember the placement indecies to repartition when we are going forward and backward. For example, first pass to find all the elements less than pivot. But we remember where place the smallest item. Second pass to find all the elements greater than pivot, but remember the larger placement pointer. Keep moving the smaller and larger placement pointer. 
+Time Complexity O(N), space complexity O(1)
+
+Forth approach: Optimized approach to 3. approach using single pass
 We will reorder the element into less than, equal to and greater than the pivot with single pass. We do this by maintaining four subarrays: bottom (element less than pivot), middle (element equals to pivot) unclassified, top (element greater than pivot). Initially, all elements are unclassified. We iterate through elements in unclassified, move elements into one of bottom, middle or top groups.
 Time Complexity O(N), space complexity O(1)
+
+Reference:
+The Dutch National Flag Problem (The Quicksort "Band-Aid")
+Back To Back SWE
+https://www.youtube.com/watch?v=ER4ivZosqCg
+
 """
 
 from typing import List
