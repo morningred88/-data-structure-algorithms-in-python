2114a06b19a7b370aa8a445b74ad3a626a18160c
Lily Ma <xma50089@gmail.com>
2021-10-11 20:03:14 -0500

    Added boolean array rearrangement with ordering

---
+++ EPI/ch5v_boolean_ordering_partition.py
@@ -0,0 +1,6 @@
+"""
+Variation problem 4:
+Given an array A of n objects with Boolean-valued key, reorder the array so that all objects false appear first.
+The relative ordering of key true should not be change.
+Use O(1) additional space and O(N) time.
+"""
