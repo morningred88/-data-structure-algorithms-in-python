f88e08f85cef9f1aa52ae7777ec4e436e9fdba22
Lily Ma <xma50089@gmail.com>
2021-10-11 20:22:41 -0500

    Added the implementation for boolean partition with ordering

--- EPI/ch5v_boolean_ordering_partition.py
+++ EPI/ch5v_boolean_ordering_partition.py
@@ -4,3 +4,23 @@ Given an array A of n objects with Boolean-valued key, reorder the array so that
 The relative ordering of key true should not be change.
 Use O(1) additional space and O(N) time.
 """
+
+from typing import List
+
+
+def rearrange_boolean_ordering(A:List[bool]) -> List:
+
+    # Initialize the placement position (index) of the 2 subarrays
+    false, true = 0, 0
+    for i in range(len(A)):
+        if A[i]:
+            true += 1
+        else:
+            A[true], A[false] = A[false], A[true]
+            false, true = false + 1, true + 1
+
+    return A
+
+
+array = [False, True, True, False]
+print(rearrange_boolean_ordering(array))
