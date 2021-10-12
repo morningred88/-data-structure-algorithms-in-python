853dd8c1d5206acd1455fa8efbbd8335898cc989
Lily Ma <xma50089@gmail.com>
2021-10-12 18:46:57 -0500

    Added implementation to arrange the array starting from the end for boolean array

--- EPI/ch5v_boolean_ordering_partition.py
+++ EPI/ch5v_boolean_ordering_partition.py
@@ -7,8 +7,7 @@ Use O(1) additional space and O(N) time.
 
 from typing import List
 
-
-def rearrange_boolean_ordering(A:List[bool]) -> List:
+def rearrange_boolean_forward(A:List[bool]) -> List:
 
     # Initialize the placement position (index) of the 2 subarrays
     false, true = 0, 0
@@ -21,6 +20,19 @@ def rearrange_boolean_ordering(A:List[bool]) -> List:
 
     return A
 
+def rearrange_boolean_backward(A:List[bool]) -> List:
+
+    # Initialize the placement position (index) of the 2 subarrays
+    false, true = len(A) - 1, len(A) - 1
+    for i in reversed(range(0, len(A))):
+        if not A[i]:
+            false -= 1
+        else:
+            A[true], A[false] = A[false], A[true]
+            false, true = false - 1, true - 1
+
+    return A
+
 
 array = [False, True, True, False]
-print(rearrange_boolean_ordering(array))
+print(rearrange_boolean_backward(array))
