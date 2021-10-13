1fdaf37cdf77c47a0f30c192dbfbd7e2f5cf234c
Lily Ma <xma50089@gmail.com>
2021-10-13 06:59:02 -0500

    Added test for forward reordering for boolean array

--- EPI/ch5v_boolean_ordering_partition.py
+++ EPI/ch5v_boolean_ordering_partition.py
@@ -7,12 +7,27 @@ Use O(1) additional space and O(N) time.
 
 from typing import List
 
+
 def rearrange_boolean_forward(A:List[bool]) -> List:
 
     # Initialize the placement position (index) of the 2 subarrays
     false, true = 0, 0
     for i in range(len(A)):
         if A[i]:
+            true += 1
+        else:
+            A[true], A[false] = A[false], A[true]
+            false, true = false + 1, true + 1
+
+    return A
+
+
+def rearrange_boolean_forward_test(A:List[int]) -> List:
+
+    # Initialize the placement position (index) of the 2 subarrays
+    false, true = 0, 0
+    for i in range(len(A)):
+        if A[i] != 0:
             true += 1
         else:
             A[true], A[false] = A[false], A[true]
@@ -51,6 +66,12 @@ def rearrange_boolean_backward_test(A:List[int]) -> List:
 
 
 array = [False, True, True, False]
-print(rearrange_boolean_backward(array))
+print(rearrange_boolean_forward(array))
 array_test =[0, 1, 2, 0]
-print(rearrange_boolean_backward_test(array_test))
+print(rearrange_boolean_forward_test(array_test))
+
+# array = [False, True, True, False]
+# print(rearrange_boolean_backward(array))
+# array_test =[0, 1, 2, 0]
+# print(rearrange_boolean_backward_test(array_test))
+
