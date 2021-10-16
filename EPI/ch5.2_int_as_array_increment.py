1eb3da98d411ba60b2f34ab59951dbcb4d34be61
Lily Ma <xma50089@gmail.com>
2021-10-16 07:44:31 -0500

    Added array increment question

---
+++ EPI/ch5.2_int_as_array_increment.py
@@ -0,0 +1,6 @@
+"""
+Question 5.2
+
+Write an non-negative integer as an array, add a given integer to the array and calculate the additional like adding it to an integer. For example, if your input is [2, 3, 5], add 15, the array should be updated to [2, 5, 0].
+"""
+
