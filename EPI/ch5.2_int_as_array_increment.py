5f348e91066e8c08f56d94fa9b15f95710b06b7f
Lily Ma <xma50089@gmail.com>
2021-10-16 08:38:08 -0500

    Added implementation for array increment

--- EPI/ch5.2_int_as_array_increment.py
+++ EPI/ch5.2_int_as_array_increment.py
@@ -1,6 +1,32 @@
 """
 Question 5.2
 
-Write an non-negative integer as an array, add a given integer to the array and calculate the additional like adding it to an integer. For example, if your input is [2, 3, 5], add 15, the array should be updated to [2, 5, 0].
+Write an non-negative integer as an array, add a given integer to the array and calculate the additional like adding it to an integer. For example, if your input is [2, 3, 9], add 1, the array should be updated to [2, 4, 0].
 """
+from typing import List
+
+
+def plus_one(array: List[int]) -> List[int]:
+
+    array[-1] += 1
+
+    for i in reversed(range(len(array))):
+        if array[i] == 10:
+            array[i] = 0
+            array[i - 1] += 1
+            if array[0] == 10:
+                array[0] = 1
+                array.append(0)
+        else:
+            break
+
+    return array
+
+
+print(plus_one([2, 3, 9]))
+print(plus_one([9, 9, 9]))
+print(plus_one([9, 9, 8]))
+
+
+
 
