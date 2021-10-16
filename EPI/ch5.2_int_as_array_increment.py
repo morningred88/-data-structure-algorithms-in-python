cb6b1688c61492c2f54e1c5477615d14838af8ef
Lily Ma <xma50089@gmail.com>
2021-10-16 09:21:51 -0500

    Added explanation to the array increment problem

--- EPI/ch5.2_int_as_array_increment.py
+++ EPI/ch5.2_int_as_array_increment.py
@@ -1,19 +1,26 @@
 """
 Question 5.2
 
-Write an non-negative integer as an array, add a given integer to the array and calculate the additional like adding it to an integer. For example, if your input is [2, 3, 9], add 1, the array should be updated to [2, 4, 0].
+Write an non-negative integer as an array, add a given integer to the array and calculate the addition like adding it to an integer. For example, if your input is [2, 3, 9], add 1, the array should be updated to [2, 4, 0].
+"""
+"""
+Explanation:
+Do the same way like we do the addition problem.
+Start from the last digit
 """
 from typing import List
 
 
 def plus_one(array: List[int]) -> List[int]:
 
+    # Start addition from the last index
     array[-1] += 1
 
     for i in reversed(range(len(array))):
         if array[i] == 10:
             array[i] = 0
             array[i - 1] += 1
+            # If the first element of array is greater than 10, append 0 to the end of array
             if array[0] == 10:
                 array[0] = 1
                 array.append(0)
